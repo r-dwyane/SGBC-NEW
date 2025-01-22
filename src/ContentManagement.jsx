b49f43bd81890d0a6a7ccfd029da81acacd7a829
Rhon Dwyane <rhon.dwyane03@gmail.com>
2025-01-23 05:47:49 +0800

    Create gh-pages branch

---
+++ src/ContentManagement.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function ContentManagement() {
+  return (
+    <div>
+    <h1>CONTENT MANAGEMENT</h1>
+    </div>
+  )
+}
+
+export default ContentManagement
---
+++ src/OrderDashboard.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function OrderDashboard() {
+  return (
+    <div>
+      <h1>ORDER DASHBOARD</h1>
+    </div>
+  )
+}
+
+export default OrderDashboard
